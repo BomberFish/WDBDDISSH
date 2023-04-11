90d0412a96c2f7e85bd299347ee4e922a8d87292
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-04-11 17:54:50 -0400

    nothing

--- gencert.sh
+++ gencert.sh
@@ -3,14 +3,14 @@ set -e
 # macOS's LibreSSL doesn't come with a openssl.crf that supports the ca command
 # Use Homebrew's instead
 sed -e "s/# copy_extensions = copy/copy_extensions = copyall/" \
-	/usr/local/etc/openssl/openssl.cnf >openssl.cnf
-OPENSSL="/usr/local/Cellar/openssl@3/3.0.7/bin/openssl"
+	/usr/local/etc/openssl@3/openssl.cnf >openssl.cnf
+OPENSSL="/usr/local/Cellar/openssl@3/3.1.0/bin/openssl"
 rm -r demoCA || true
 mkdir -p demoCA/newcerts || true
 echo 01 > demoCA/serial
 touch demoCA/index.txt
 "$OPENSSL" req -newkey rsa:1024 -keyout key.pem -out req.pem -sha256 -nodes \
-	-subj "/C=CA/ST=BC/O=Worth Doing Badly/CN=Worth Doing Badly DDI" \
+	-subj "/C=CA/ST=BC/O=J.C. Jensen in Spaaaaace!!!!/CN=AbsoluteSolver DDI" \
 	-addext "basicConstraints=critical, CA:true"
 "$OPENSSL" ca -batch -selfsign -keyfile key.pem -in req.pem -out cert_raw.pem -startdate 070416225531Z -enddate 140416225531Z \
 	-config openssl.cnf
